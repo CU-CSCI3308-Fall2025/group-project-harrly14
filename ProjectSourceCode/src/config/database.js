087157b4022d21524071399d509d958a9af4b2d2
Sully <sharrer619@gmail.com>
2025-11-20 15:27:07 -0700

    Revert "Update files for Render compatibility"
    This reverts commit 7aa7684094506c2f9ebe497033bab6922d985987.

--- ProjectSourceCode/src/config/database.js
+++ ProjectSourceCode/src/config/database.js
@@ -1,18 +1,12 @@
 const pgp = require('pg-promise')();
 
-const dbConfig = process.env.DATABASE_URL
-  ? {
-      connectionString: process.env.DATABASE_URL,
-      // Render requires SSL
-      ssl: { rejectUnauthorized: false }
-    }
-  : {
-      host: 'db',
-      port: 5432,
-      database: process.env.POSTGRES_DB,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-    };
+const dbConfig = {
+  host: 'db',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
 
 const db = pgp(dbConfig);
 
