9f59dc2579021ecdcb0be6f36fc7cc35bcdc763b
Sully <sharrer619@gmail.com>
2025-11-20 15:21:03 -0700

    Update script to use DATABASE_URL

--- ProjectSourceCode/src/init_data/wait_for_db.js
+++ ProjectSourceCode/src/init_data/wait_for_db.js
@@ -3,13 +3,17 @@ const MAX_RETRIES = 30;
 const RETRY_DELAY = 1000;
 
 async function waitForDB() {
-  const client = new Client({
-    host: 'db',
-    port: 5432,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-  });
+  const clientConfig = process.env.DATABASE_URL
+    ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+    : {
+        host: 'db',
+        port: 5432,
+        database: process.env.POSTGRES_DB,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+      };
+
+  const client = new Client(clientConfig);
   
   let retries = MAX_RETRIES;
   while (retries > 0) {
