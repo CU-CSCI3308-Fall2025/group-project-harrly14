35a1de6c8153125d09613f097d09a0e45d837d3d
Xander DuBois <aldu9080@colorado.edu>
2025-12-02 03:21:39 -0700

    removed extra console.log statements I forgot about

--- ProjectSourceCode/src/routes/report.js
+++ ProjectSourceCode/src/routes/report.js
@@ -8,7 +8,6 @@ const lot_full_lifespan = '1 hour'; //how long a "lot full" report should remain
 
 router.post('/create', isAuthenticated, async (req, res) => {
     
-    console.log("ding dong");
 
     try {
 
@@ -20,18 +19,12 @@ router.post('/create', isAuthenticated, async (req, res) => {
 
         let success_message = 'Report submitted successfully'
 
-        //console.log(test);
-
-        //console.log(test[0].num_reports);
-
         const num_reports = db_result[0].num_reports;
 
         if(num_reports > 3) {
-            console.log('lotta reports')
             await db.none('DELETE FROM reports WHERE time = (SELECT MIN(time) FROM reports WHERE user_id = $1);', [userId]);
             success_message = 'Report submitted - oldest report removed'
         } else {
-            console.log('notta lotta reports')
         }
 
 
@@ -51,15 +44,9 @@ router.post('/create', isAuthenticated, async (req, res) => {
 async function cleanupReports() {
     console.log("cleaning database");
 
-    const db_result1 = await db.query('SELECT * FROM reports WHERE (time < (NOW()-INTERVAL $1)) AND report_type = \'Parking Services\'', [parking_services_lifespan])
     await db.none('DELETE FROM reports WHERE (time < (NOW()-INTERVAL $1)) AND report_type = \'Parking Services\'', [parking_services_lifespan])
-    console.log(db_result1)
 
-    const db_result2 = await db.query('SELECT * FROM reports WHERE (time < (NOW()-INTERVAL $1)) AND report_type = \'Parking Lot Full\'', [lot_full_lifespan])
-    console.log(db_result2)
-    await db.none('DELETE FROM reports WHERE (time < (NOW()-INTERVAL $1)) AND report_type = \'Parking Services\'', [parking_services_lifespan])
-    
-    
+    await db.none('DELETE FROM reports WHERE (time < (NOW()-INTERVAL $1)) AND report_type = \'Parking Services\'', [lot_full_lifespan])
 }
 
 module.exports = router;
